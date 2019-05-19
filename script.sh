5460d9722cb102b97e9e3f691a4c22f67f66e4ca
Jan Mikeš <j.mikes@me.com>
2019-05-19 22:32:29 +0200

    Idea

--- script.sh
+++ script.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+orig_branch=git rev-parse --abbrev-ref HEAD
+new_branch=${orig_branch}-fix
+
 # Github API - Check if brach exists
 
-git checkout -b test
+git checkout -b ${new_branch}
 echo 'Test' > test.txt
 git add .
 git commit -m "Test commit"
-git push -u
+git push --set-upstream origin ${new_branch}
 
-# create pull request
+# create pull request from $branch to $originalBranch
