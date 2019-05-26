7d8f67e578f989e4dcfe36ddcb4018afa3d6a467
Jan Mikeš <j.mikes@me.com>
2019-05-26 23:28:25 +0200

    First step - auth app

--- src/Controller/GitHubWebHookController.php
+++ src/Controller/GitHubWebHookController.php
@@ -2,6 +2,9 @@
 
 namespace RectorCI\Controller;
 
+use Firebase\JWT\JWT;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,6 +15,32 @@ final class GitHubWebHookController
      */
     public function __invoke(): Response
     {
-        return new Response();
+        $client = new Client();
+
+        // 1. Authenticating as a GitHub App - https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-a-github-app
+        $privateKey = file_get_contents(__DIR__ . '/../../config/keys/rector-ci.pem');
+        $token = array(
+            'iss' => getenv('GITHUB_APP_ID'),
+            'exp' => time() + (10 * 60),
+            'iat' => time(),
+        );
+
+        $jwt = JWT::encode($token, $privateKey, 'RS256');
+
+        // curl -i -H "Authorization: Bearer YOUR_JWT" -H "Accept: application/vnd.github.machine-man-preview+json" https://api.github.com/app
+        $response = $client->request('GET', 'https://api.github.com/app', [
+            RequestOptions::HEADERS => [
+                'Accept' => 'application/vnd.github.machine-man-preview+json',
+                'Authorization' => sprintf('Bearer %s', $jwt),
+            ]
+        ]);
+
+        $response = $response->getBody()->getContents();
+
+        // 2. Authenticating as an installation - https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-an-installation
+
+        // 3. git clone https://x-access-token:<token>@github.com/owner/repo.git
+
+        return new Response($response);
     }
 }
