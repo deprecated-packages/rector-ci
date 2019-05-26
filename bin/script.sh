fec03cd9eb353bfc2247305327006b98bc388c33
Jan Mikeš <j.mikes@me.com>
2019-05-27 00:49:58 +0200

    Opening PR in name of Rector CI :-)

--- bin/script.sh
+++ bin/script.sh
@@ -11,16 +11,14 @@
 # 8. push
 # 9. open PR
 
-orig_branch=$(git rev-parse --abbrev-ref HEAD)
-new_branch=${orig_branch}-rector
+orig_branch=${1}
+new_branch=${2}
 
 # Github API - Check if brach exists
 # If fix PR already exists, should it edit existing PR with new commit? or delete it and replace?
 
 git checkout -b ${new_branch}
-echo 'Test' > ${1}.txt # For test purposes, argument of bash script
+echo 'Test' > Somefile.txt # For test purposes, argument of bash script
 git add .
 git commit -m "Test commit"
 git push --set-upstream origin ${new_branch}
-
-php open-github-pr.php ${orig_branch} ${new_branch}
--- src/Controller/GitHubWebHookController.php
+++ src/Controller/GitHubWebHookController.php
@@ -5,6 +5,9 @@ namespace RectorCI\Controller;
 use Firebase\JWT\JWT;
 use GuzzleHttp\Client;
 use GuzzleHttp\RequestOptions;
+use Nette\Utils\Json;
+use Nette\Utils\Strings;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,11 +16,15 @@ final class GitHubWebHookController
     /**
      * @Route("/web-hooks/github", methods={"POST"})
      */
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
+        // TODO: Validate if event is check_suite
+        //$request->headers->get('X-Github-Event')
+
+        // @TODO!! Very important! Prevent circular reference, only commits by human
+
         $client = new Client();
 
-        // 1. Authenticating as a GitHub App - https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-a-github-app
         $privateKey = file_get_contents(__DIR__ . '/../../config/keys/rector-ci.pem');
         $token = array(
             'iss' => getenv('GITHUB_APP_ID'),
@@ -25,22 +32,47 @@ final class GitHubWebHookController
             'iat' => time(),
         );
 
+        $data = Json::decode($request->getContent());
+        $installationId = $data->installation->id;
+
         $jwt = JWT::encode($token, $privateKey, 'RS256');
 
-        // curl -i -H "Authorization: Bearer YOUR_JWT" -H "Accept: application/vnd.github.machine-man-preview+json" https://api.github.com/app
-        $response = $client->request('GET', 'https://api.github.com/app', [
+        $response = $client->request('POST', "https://api.github.com/app/installations/$installationId/access_tokens", [
             RequestOptions::HEADERS => [
                 'Accept' => 'application/vnd.github.machine-man-preview+json',
                 'Authorization' => sprintf('Bearer %s', $jwt),
             ]
         ]);
 
-        $response = $response->getBody()->getContents();
+        $responseData = Json::decode($response->getBody()->getContents());
+        $cloneToken = $responseData->token;
+        $repositoryName = $data->repository->full_name;
+        $cloneUrl = $data->repository->clone_url;
+        $cloneUrl = sprintf('https://x-access-token:%s@', $cloneToken) . Strings::after($cloneUrl, 'https://');
 
-        // 2. Authenticating as an installation - https://developer.github.com/apps/building-github-apps/authenticating-with-github-apps/#authenticating-as-an-installation
+        $originalBranch = $data->check_suite->head_branch;
+        $newBranch = $originalBranch . '-rector';
 
-        // 3. git clone https://x-access-token:<token>@github.com/owner/repo.git
+        shell_exec("git clone $cloneUrl ../repositories/$repositoryName");
+        shell_exec("cd ../repositories/$repositoryName && ../../../bin/script.sh $originalBranch $newBranch");
 
-        return new Response($response);
+        // @TODO now it commits as user, maybe github app should commit instead??
+
+        $client = new Client();
+        $client->request('POST', "https://api.github.com/repos/$repositoryName/pulls", [
+            RequestOptions::HEADERS => [
+                'Accept' => 'application/vnd.github.v3+json',
+                'Authorization' => sprintf('Token %s', $cloneToken),
+                'Content-Type' => 'application/json',
+            ],
+            RequestOptions::BODY => Json::encode($body = [
+                'title' => 'Rector - Fix',
+                'head' => $newBranch,
+                'base' => $originalBranch,
+                'body' => 'Rector automated pull request',
+            ]),
+        ]);
+
+        return new Response($cloneUrl);
     }
 }
