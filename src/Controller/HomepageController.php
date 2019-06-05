970bf33d01e53fede8d36fb9ecc55d165430517c
Jan Mikeš <j.mikes@me.com>
2019-06-06 00:21:35 +0200

    Homepage

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,18 @@
+<?php declare (strict_types=1);
+
+namespace Rector\RectorCI\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+final class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/", methods={"GET"})
+     */
+    public function __invoke(): Response
+    {
+        return $this->render('homepage/homepage.twig');
+    }
+}
